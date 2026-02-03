d74b257858a7ca0aef4262a324ece1d6140dab88
Sanjar Yusupjonov <s.yusupjonov@newuu.uz>
2026-02-03 22:42:38 +0500

    initial commit/ project creation

---
+++ src/main/java/com/safenet/backend/SafenetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.safenet.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafenetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafenetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safenet/backend/SafenetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safenet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafenetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
